76d98e74b3bf69f73986f7631d8238e13f8dbc09
sunbetkapper@gmail.com <napvital@gmail.com>
2020-04-26 19:32:35 +0200

    First Commit. I am going to implement redux-saga middleware instead of redux-thunk for asynchronues request;

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,79 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyDsIp_UA7eH09uwiOqvWem0beP-eTjGhlg",
+   authDomain: "virtus-db.firebaseapp.com",
+   databaseURL: "https://virtus-db.firebaseio.com",
+   projectId: "virtus-db",
+   storageBucket: "virtus-db.appspot.com",
+   messagingSenderId: "858459877295",
+   appId: "1:858459877295:web:bdbc24edcacab1a7ed895a",
+   measurementId: "G-L2JBE1Y1L8"
+};
+
+firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
+export const addCollectionAndDocuments = async(collectionKey, objectsToAdd) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+  return await batch.commit()
+}
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map( doc => {
+    const {title, items} = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+       items
+    }
+  }
+  );
+ return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {}) 
+}
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
